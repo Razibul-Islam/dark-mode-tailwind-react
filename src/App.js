f1a02432e3680744d8a25ed598836c10b774c165
Razibul Islam <razibulislam665@gmail.com>
2022-12-12 03:02:53 +0600

    react-tailwind-dark-mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
+  // This is default windows theme detected
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+// This is user toggle change
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      // console.log(document.documentElement);
+    } else {
+      document.documentElement.classList.remove("dark");
+      // console.log(document.documentElement);
+    }
+  }, [theme]);
+
+  // This onclick toggle
+
+  const handleDark = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white dark:bg-black h-screen">
+      <p
+        onClick={handleDark}
+        className="text-dark dark:text-white flex justify-center items-center h-full text-7xl"
+      >
+        Hey i am dark mode
+      </p>
     </div>
   );
 }
